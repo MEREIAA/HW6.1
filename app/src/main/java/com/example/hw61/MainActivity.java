280728c6904028895b5ef8a0d1820d7b6bd11465
MEREIAA <mereiaalmazbekowa@gmail.com>
2024-11-26 18:16:19 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw61/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.hw61;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.hw61.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Picasso.get()
+                .load("https://static.wikia.nocookie.net/vsbattles/images/0/03/Seems_fine.jpg/revision/latest/scale-to-width-down/600?cb=20210409204243")
+                .into(binding.imageView);
+
+        binding.imageView.setOnClickListener(v ->  {
+
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/hw61/MapsActivity.java
@@ -0,0 +1,43 @@
+package com.example.hw61;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+    mMap = googleMap;
+    LatLng kyrgyzstan = new LatLng(41.0, 74.0);
+    mMap.addMarker(new MarkerOptions()
+            .position(kyrgyzstan)
+            .title("Marker in kyrgyzstan"));
+    mMap.moveCamera(CameraUpdateFactory.newLatLng(kyrgyzstan));
+    }
+}
